734caf08caad71283a8f96d7b0675a413224c2f9
Maxwell Adwale <48594933+maxwelladwale@users.noreply.github.com>
2023-01-15 22:54:41 +0300

    first commit

---
+++ moviesapp/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+Route::get('/', [MoviesController::class, 'index']);
+Route::get('/movies/{id}', [MoviesController::class, 'show']);
+
+// Route::get('/', 'MoviesController@index')->name('movies.index');
+// Route::get('/', 'UserController@index')->name('user');
+// Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
+
+// Route::view('/', 'index');
+// Route::view('/movies', 'show');
